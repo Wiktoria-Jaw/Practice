12a4f9ec5a64f26226df1be6d7f43d5ed2c93065
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-05 10:42:46 +0100

    Updated PraktykiAPI with new Controller and made working connection with database

---
+++ PraktykiAPI/PraktykiAPI/Models/Day_Off.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PraktykiAPI.Models
+{
+    //[Table("Days_Off")]
+    public class Day_Off
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public DateOnly Start_Date { get; set; }
+        [Required]
+        public DateOnly End_Date { get; set; }
+        public string Status { get; set; } = "pending";
+        [Required]
+        public int Employee_ID { get; set; }
+
+        [ForeignKey("Employee_ID")]
+        public Employee Employee { get; set; }
+    }
+}
