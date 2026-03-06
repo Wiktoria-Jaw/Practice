0409ab7a95ff93236b036afe92cf7a2016a89885
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-06 15:15:21 +0100

    Updated React practice, this one was static side.

--- Nauka_React/react-tutorial/src/components/Header.jsx
+++ Nauka_React/react-tutorial/src/components/Header.jsx
@@ -1,13 +1,9 @@
 export default function Header(){
     return(
-        <header className="main-header">
-            <img src=".\src\assets\react_logo.png" className="logo" alt="React logo"/>
+        <header>
             <nav>
-                <ul className = "nav-list">
-                    <li className="nav-list-item">Pricing</li>
-                    <li className="nav-list-item">About</li>
-                    <li className="nav-list-item">Contact</li>
-                </ul>
+                <img src=".\src\assets\react_logo.png" alt="React logo"/>
+                <span>ReactFacts</span>
             </nav>
         </header>
     )
--- Nauka_React/react-tutorial/src/components/MainContent.jsx
+++ Nauka_React/react-tutorial/src/components/MainContent.jsx
@@ -1,12 +1,14 @@
 export default function MainContent(){
     return(
         <main>
-           <h1>React</h1>
-                <ol>
-                    <li>smth</li>
-                    <li>smth2</li>
-                    <li>smth3</li>
-                </ol>
+           <h1>Fun facts about React</h1>
+                <ul className="factsList">
+                    <li>Was first Rreleased in 2013</li>
+                    <li>Was originally created by Jordan Walke</li>
+                    <li>Has well over 100K stars on GitHub</li>
+                    <li>Is maintained by Meta</li>
+                    <li>Powers thousands of enterprise apps, including mobile apps</li>
+                </ul>
         </main>     
     )
 }
