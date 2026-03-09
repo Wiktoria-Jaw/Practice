ec93e8a792e3bc8a52c714ed78199b140342103d
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-09 12:34:20 +0100

    Added frontend site (without connection to API)

---
+++ PraktykiReact/src/components/Header.jsx
@@ -0,0 +1,12 @@
+export default function Header(){
+    return(
+        <header>
+            <h1>Hello [Imie pracownika]</h1>
+            <div className="menu">
+                <a href="#">Home</a>
+                <a href="#">Calendar</a>
+                <a href="#">Log out</a>
+            </div>
+        </header>
+    )
+}
