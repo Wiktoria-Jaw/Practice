3eaacdb93425f5164cbc1ce57e8cfd1aed0a268d
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-19 14:51:09 +0100

    Added User controller that handle login and logout

---
+++ PraktykiAPI/PraktykiAPI/Controllers/UsersController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PraktykiAPI.Models;
+
+namespace PraktykiAPI.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly PasswordHasher<User> _hasher;
+
+        public UsersController(AppDbContext context)
+        {
+            _context = context;
+            _hasher = new PasswordHasher<User>();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginRequest request)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(u => u.Login == request.Login);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Incorrect login or password." });
+            }
+
+            var result = _hasher.VerifyHashedPassword(user, user.Password, request.Password);
+            if(result == PasswordVerificationResult.Failed)
+            {
+                return BadRequest(new { message = "Incorrect login or password." });
+            }
+
+            if (user.IsActive == 0)
+            {
+                return BadRequest(new {message = "This user isn't an active user."});
+            }
+
+            user.IsLogIn = 1;
+            await _context.SaveChangesAsync();
+
+            return Ok(new { id=user.EmployeeID, permission=user.Permission, login=user.Login });
+        }
+
+        public class LoginRequest
+        {
+            public string Login { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPut("logout/{emplID}")]
+        public async Task<IActionResult> Logout(int emplID)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.EmployeeID == emplID);
+
+            if(user == null)
+            {
+                return BadRequest(new {message="User not found."});
+            }
+
+            user.IsLogIn = 0;
+            await _context.SaveChangesAsync();
+
+            return Ok(new { message = "Sucessfully log out." });
+        }
+
+        private bool UserExists(int id)
+        {
+            return _context.Users.Any(e => e.ID == id);
+        }
+    }
+}
