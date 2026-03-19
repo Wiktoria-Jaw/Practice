e52a0a19dcda966752867ef7a5f6dbd855f5fe61
Wiktoria <wiktoriaajaworowska@gmail.com>
2026-03-19 12:41:00 +0100

    Created new table with work settings, added new migration to add it, added new employee and settings to DbInitializator

---
+++ PraktykiAPI/PraktykiAPI/Models/WorkSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace PraktykiAPI.Models
+{
+    [Table("WorkSettings")]
+    public class WorkSettings
+    {
+        [Key]
+        public int ID { get; set; }
+        public int? MinWorkdayLengthInMinutes { get; set; }
+        public int? AutoEndWorkdayLengthInMinutes { get; set; }
+        public int? MinBreakBetweenWorkdaysInMinutes { get; set; }
+        public int? MinWorkdayLengthForBreakInMinutes { get; set; }
+        public int? MinBreakLengthInMinutes { get; set; }
+    }
+}
